0d656ae5f6b7bd80f0001e5f7686eef0db934847
Fenz <uukooo@126.com>
2017-11-17 00:21:19 +0800

    work-pad

---
+++ src/components/work-pad.vue
@@ -0,0 +1,54 @@
+<template>
+	<aside class="work-pad">
+		<key-button key-group="c_0" @press="creating= true">Create</key-button>
+		<footer>
+			<creator v-if="creating" @create="create" @cancel="creating= false"></creator>
+		</footer>
+	</aside>
+</template>
+
+<script>
+	import keyButton from './key-button';
+	import creator from './creator';
+
+	export default {
+
+		name: 'work-pad',
+
+		components:
+		{
+			keyButton,
+			creator,
+		},
+
+		methods:
+		{
+			create( element )
+			{
+				this.creating= false;
+				z( 'creating a '+element )
+			}
+		},
+
+		data()
+		{
+			return {
+				creating: false,
+			};
+		},
+
+	};
+</script>
+
+<!-- layout -->
+	<style>
+		.work-pad
+		{ padding:1em; }
+	</style>
+<!-- /layout -->
+
+<!-- theme -->
+	<style>
+
+	</style>
+<!-- /theme -->
