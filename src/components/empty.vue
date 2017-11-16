b7591bc7828befb2ead53781610c8c3c85f0ed6f
Fenz <uukooo@126.com>
2017-11-17 00:21:53 +0800

    add a template for fast build vue component

---
+++ src/components/empty.vue
@@ -0,0 +1,29 @@
+<template>
+
+</template>
+
+<script>
+	export default {
+
+		name: '',
+
+		data()
+		{
+			return {
+			};
+		},
+
+	};
+</script>
+
+<!-- layout -->
+	<style>
+
+	</style>
+<!-- /layout -->
+
+<!-- theme -->
+	<style>
+
+	</style>
+<!-- /theme -->
