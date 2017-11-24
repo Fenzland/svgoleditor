ea0cddeea4682757014f81b6f9989d604370b6fa
Fenz <uukooo@126.com>
2017-11-24 08:05:42 +0800

    abstract the masked layer from creator layer into a stand alone component

--- src/components/creator.vue
+++ src/components/creator.vue
@@ -1,16 +1,17 @@
 <template>
-	<div class="creator">
+	<masked-layer>
 		<key-button key-group="p_0" @press="create( 'Path' )">Path</key-button>
 		<key-button key-group="l_0" @press="create( 'Line' )">Line</key-button>
 		<key-button key-group="r_0" @press="create( 'Rect' )">Rect</key-button>
 		<key-button key-group="c_0" @press="create( 'Circle' )">Circle</key-button>
 		<key-button key-group="e_0" @press="create( 'Ellipse' )">Ellipse</key-button>
 		<key-button key-group="Escape_0" @press="cancel">cancel</key-button>
-	</div>
+	</masked-layer>
 </template>
 
 <script>
 	import keyButton from './key-button';
+	import maskedLayer from './tools/masked-layer';
 
 	export default {
 
@@ -19,6 +20,7 @@
 		components:
 		{
 			keyButton,
+			maskedLayer,
 		},
 
 		methods:
@@ -46,30 +48,11 @@
 <!-- layout -->
 	<style>
 
-		.creator
-		{ position:fixed; z-index:8; top:0; left:0; right:0; bottom:0; margin:auto; width:fit-content; height:fit-content; padding:2em; }
-
-		.creator::before
-		{ content:''; display:block; position:fixed; z-index:-1; top:0; left:0; right:0; bottom:0; }
-
-		.creator::after
-		{ content:''; display:block; position:absolute; z-index:-1; top:0; left:0; right:0; bottom:0; }
-
-
 	</style>
 <!-- /layout -->
 
 <!-- theme -->
 	<style>
-
-		.creator
-		{ box-shadow:0 0.2vh 0.5vw hsla(0,0%,0%,.25) }
-
-		.creator::before
-		{ background-color:hsla(0,0%,0%,0.5); }
-
-		.creator::after
-		{ border-radius:1em; background-color:hsl(0,0%,100%); }
 
 	</style>
 <!-- /theme -->
---
+++ src/components/tools/masked-layer.vue
@@ -0,0 +1,76 @@
+<template>
+	<div :is="tag" class="masked-layer">
+		<slot></slot>
+	</div>
+</template>
+
+<script>
+	export default {
+
+		name: '',
+
+		props:
+		{
+			tag:
+			{
+				type: String,
+				default: 'div',
+			},
+		},
+
+		data()
+		{
+			return {
+			};
+		},
+
+	};
+</script>
+
+<!-- layout -->
+	<style>
+
+		.masked-layer
+		{ position:fixed; z-index:8; top:0; left:0; right:0; bottom:0; margin:auto; width:fit-content; height:fit-content; padding:2em; }
+
+		.masked-layer::before
+		{ content:''; display:block; position:fixed; z-index:-1; top:0; left:0; right:0; bottom:0; }
+
+		.masked-layer::after
+		{ content:''; display:block; position:absolute; z-index:-1; top:0; left:0; right:0; bottom:0; }
+
+	</style>
+<!-- /layout -->
+
+<!-- theme -->
+	<style>
+
+		.masked-layer
+		{
+			--masked-bg-alpha: 1.0;
+			--mask-bg-alpha:   0.5;
+			--masked-bg-hue: 0;
+			--mask-bg-hue:   0;
+			--masked-bg-saturation: 000%;
+			--mask-bg-saturation:   000%;
+			--masked-bg-luminosity: 100%;
+			--mask-bg-luminosity:   000%;
+			--masked-bg-color: hsla(var(--masked-bg-hue),var(--masked-bg-saturation),var(--masked-bg-luminosity),var(--masked-bg-alpha));
+			--mask-bg-color:   hsla(var(--mask-bg-hue),var(--mask-bg-saturation),var(--mask-bg-luminosity),var(--mask-bg-alpha));
+
+			--fillet: 1em;
+
+			--float-z: 1;
+		}
+
+		.masked-layer
+		{ border-radius:var(--fillet); box-shadow:0 calc(var(--float-z)*var(--float-z)*0.2vh) calc(var(--float-z)*0.5vw) hsla(0,0%,0%,.25); }
+
+		.masked-layer::before
+		{ background-color:var(--mask-bg-color); }
+
+		.masked-layer::after
+		{ border-radius:var(--fillet); background-color:var(--masked-bg-color); }
+
+	</style>
+<!-- /theme -->
